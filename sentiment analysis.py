291cfef538b72e12c95d50b29c9ac2459ff0e9c4
pushpak-2099 <pushpakdighore@gmail.com>
2026-02-06 23:58:57 +0530

    making some changes after a review

--- sentiment analysis.py
+++ sentiment analysis.py
@@ -9,9 +9,9 @@ while user_input.lower() !="stop":
   sentence=TextBlob(user_input) 
   if sentence.sentiment.polarity >=0.1 :
        print("the sentence is positive. ")
-  elif sentence.sentiment.polarity >=-0.1 or sentence.sentiment.polarity <=0.1 :
+  elif -0.1 <= sentence.sentiment.polarity <= 0.1:
        print("the sentence is neutral.") 
-  else : print (" the sentence is negative.")
+  else  print (" the sentence is negative.")
      
   user_input=input("enter another statement: ")
 
